79c1be26642cbc8fdebabc384df0d620b3884c51
Konstantin Lukinskikh <lumrandir@lumrandir.su>
2019-10-09 16:26:46 +0500

    Implement code acquiring

--- Auth.fs
+++ Auth.fs
@@ -1,52 +1,99 @@
 module Auth
 
+open System
 open System.IO
 open System.Net
+open System.Net.Sockets
 open System.Text.RegularExpressions
+open System.Web
 
 let (|IniValue|_|) pattern input =
   let m = Regex.Match(input, "^" + pattern + "=(.+)$")
   if (m.Success) then Some m.Groups.[1].Value else None
 
 type AuthConfig =
-  { client_id     : string
-    response_type : string
-    redirect_uri  : string
-    scope         : string
+  { clientId     : string
+    responseType : string
+    redirectUri  : string
+    scope        : string
   }
 
   member this.urlenc : string =
-    [ "client_id=" + WebUtility.UrlEncode(this.client_id)
-    ; "response_type=" + WebUtility.UrlEncode(this.response_type)
-    ; "redirect_uri=" + WebUtility.UrlEncode(this.redirect_uri)
+    [ "client_id=" + WebUtility.UrlEncode(this.clientId)
+    ; "response_type=" + WebUtility.UrlEncode(this.responseType)
+    ; "redirect_uri=" + WebUtility.UrlEncode(this.redirectUri)
     ; "scope=" + WebUtility.UrlEncode(this.scope)
     ] |> String.concat "&"
 
-  static member default_config : AuthConfig =
-    { client_id = ""
-    ; response_type = "code"
-    ; redirect_uri = "http://localhost:8080"
+  static member defaultConfig : AuthConfig =
+    { clientId = ""
+    ; responseType = "code"
+    ; redirectUri = "http://localhost:8080"
     ; scope = "offline_access onedrive.readwrite"
     }
 
   static member load (path : string) : AuthConfig =
     let ini = File.ReadLines(path)
-    let client_id =
+    let clientId =
       Seq.pick (fun line ->
                   match line with
                   | IniValue "client_id" value -> Some value
                   | _ -> None
       ) ini
 
-    { AuthConfig.default_config with client_id = client_id }
+    { AuthConfig.defaultConfig with clientId = clientId }
 
+let private acceptClient (client : TcpClient) handler =
+  use stream = client.GetStream()
+  use reader = new StreamReader(stream)
+  let header = reader.ReadLine()
+  if not (String.IsNullOrEmpty(header)) then
+    use writer = new StreamWriter(stream)
+    let result = handler (header, writer)
+    writer.Flush()
+    result
+  else
+    ""
 
-let private auth_server_url : string =
+let private authServerUrl =
   "https://login.live.com/oauth20_authorize.srf"
 
-let private auth_url (config : AuthConfig) : string =
-  auth_server_url + "?" + config.urlenc
+let private authUrl (config : AuthConfig) : string =
+  authServerUrl + "?" + config.urlenc
 
-let print_auth_url (config : AuthConfig) : unit =
-  printfn "Get the code here: %s" (auth_url config)
+let private printAuthUrl (config : AuthConfig) : unit =
+  printfn "Get the code here: %s" (authUrl config)
 
+let private onCallback codeFilePath (header : string, writer : IO.StreamWriter) =
+  let split = header.Split(" ")
+  let path = split.[1]
+  let code = HttpUtility.ParseQueryString(path).Item(0)
+  writer.Write("HTTP/1.1 200 OK\r\n\r\n")
+  writer.Write("Got it.")
+  File.WriteAllText(codeFilePath, code)
+  code
+
+let private startCallbackServer (addr : string, port : int) handler =
+  let ip = IPAddress.Parse(addr)
+  let listener = TcpListener(ip, port)
+  listener.Start()
+  let client = listener.AcceptTcpClient()
+  acceptClient client handler
+
+let private requestAuthCode home handler =
+  let path = home + "/.zerodrive/zerodriverc"
+  if not <| File.Exists(path) then
+    printfn "Configuration file not found!"
+    exit(1)
+  else
+    let config = AuthConfig.load(path)
+    printAuthUrl config
+    startCallbackServer ("127.0.0.1", 8080) handler
+
+let authenticate =
+  let home = Environment.GetEnvironmentVariable("HOME")
+  let codeFilePath = home + "/.zerodrive/code"
+  if not <| File.Exists(codeFilePath) then
+    requestAuthCode home (onCallback codeFilePath)
+  else
+    File.ReadAllText(codeFilePath)
--- Program.fs
+++ Program.fs
@@ -1,13 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open Auth
+open System
 
 [<EntryPoint>]
 let main argv =
-  { client_id = "623570b4-9363-4a1c-b5a9-f7328ad4e5d3"
-  ; response_type = "code"
-  ; redirect_uri = "http://localhost:8080"
-  ; scope = "offline_access onedrive.readwrite"
-  } |> Auth.print_auth_url
+  let code = authenticate
+  printfn "code: %s" code
   0
